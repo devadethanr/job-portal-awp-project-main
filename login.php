84b98641acadba50392357151c38aacedbd72224
devadethanr <125908755+devadethanr@users.noreply.github.com>
2023-08-20 20:35:41 +0530

    added admin and applicant pages

--- login.php
+++ login.php
@@ -1,13 +1,77 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>login</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+        <style>
+            .divider:after,
+            .divider:before {
+            content: "";
+            flex: 1;
+            height: 1px;
+            background: #eee;
+            }
+        </style>
+    </head>
+        <body>
+        <section class="vh-100">
+        <div class="container py-5 h-100">
+            <div class="row d-flex align-items-center justify-content-center h-100">
+            <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+            <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign in to Job portal</p>
+                <form method="POST">
+                    <!-- Email input -->
+                    <div class="form-outline mb-4">
+                        <input type="email" id="form1Example13" name="mail" class="form-control form-control-lg" />
+                        <label class="form-label" for="form1Example13">Email address</label>
+                    </div>
+
+                    <!-- Password input -->
+                    <div class="form-outline mb-4">
+                        <input type="password" id="form1Example23" name="pass" class="form-control form-control-lg" />
+                        <label class="form-label" for="form1Example23">Password</label>
+                    </div>
+
+                    <div class="d-flex justify-content-around align-items-center mb-4">
+                        <!-- Checkbox -->
+                        <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
+                        </div>
+                        <a href="#!">Forgot password?</a>
+                    </div>
+
+                    <!-- Submit button -->
+                    <button name="l_btn" type="submit"  class="btn btn-primary btn-lg btn-block"> sign in </button>
+                </form>
+            </div>
+            </div>
+        </div>
+        </section>
+    </body>
 </html>
+<?php
+    if(isset($_POST['l_btn'])){
+        require "conn_db.php";
+        $mail=$_POST['mail'];
+        $pass=$_POST['pass'];
+        $l_qry="SELECT * FROM `register` WHERE `mail`= '$mail' AND `pass` = '$pass'";
+        $l_rs=mysqli_query($conn,$l_qry);
+        while($row=mysqli_fetch_array($l_rs)){
+            session_start();
+            if($row['role']=='ADMIN-JOB REGISTER'){
+                $_SESSION["sid"]=$row['mail'];
+                header("location:admin.php");
+            }
+            elseif($row['role']=='APPLICANT'){
+                $_SESSION["sid"]=$row['mail'];
+                header("location:applicant.php");
+            }
+            else{
+                echo "<script> alert('account not found or suspended') </script>";
+            }
+        }
+    }
+?>
--- signup.php
+++ signup.php
@@ -1,91 +1,92 @@
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>register-user</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-</head>
-<body>
-    <section class="vh-100" style="background-color: #eee;">
-    <div class="container h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-lg-12 col-xl-11">
-            <div class="card text-black" style="border-radius: 25px;">
-            <div class="card-body p-md-5">
-                <div class="row justify-content-center">
-                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>register-user</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <section class="vh-100" style="background-color: #eee;">
+        <div class="container h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-lg-12 col-xl-11">
+                <div class="card text-black" style="border-radius: 25px;">
+                <div class="card-body p-md-5">
+                    <div class="row justify-content-center">
+                    <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+                        <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up for Job portal</p>
 
-                    <form class="mx-1 mx-md-4">
+                        <form method= "POST" class="mx-1 mx-md-4">
 
-                    <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                        <input type="text" id="form3Example1c" name="name" class="form-control" required/>
-                        <label class="form-label" for="form3Example1c">Your Name</label>
+                        <div class="d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                            <div class="form-outline flex-fill mb-0">
+                            <input type="text" id="form3Example1c" name="name" class="form-control" required/>
+                            <label class="form-label" for="form3Example1c">Your Name</label>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                        <input type="email" id="form3Example3c" name="mail" class="form-control" required/>
-                        <label class="form-label" for="form3Example3c">Your Email</label>
+                        <div class="d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                            <div class="form-outline flex-fill mb-0">
+                            <input type="email" id="form3Example3c" name="mail" class="form-control" required/>
+                            <label class="form-label" for="form3Example3c">Your Email</label>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                        <input type="password" id="form3Example4c" name="pass" class="form-control" required />
-                        <label class="form-label" for="form3Example4c">Password</label>
+                        <div class="d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                            <div class="form-outline flex-fill mb-0">
+                            <input type="password" id="form3Example4c" name="pass" class="form-control" required />
+                            <label class="form-label" for="form3Example4c">Password</label>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                        <input type="password" id="form3Example4cd" name="rpass" class="form-control" required/>
-                        <label class="form-label" for="form3Example4cd">Repeat your password</label>
+                        <div class="d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                            <div class="form-outline flex-fill mb-0">
+                            <input type="password" id="form3Example4cd" name="rpass" class="form-control" required/>
+                            <label class="form-label" for="form3Example4cd">Repeat your password</label>
+                            </div>
                         </div>
+
+                        <div class="form-check d-flex justify-content-center mb-5">
+                            <select class="form-select"  name="role" aria-label="Default select example" required>
+                                <option selected>Create account as</option>
+                                <option value="ADMIN-JOB REGISTER">ADMIN-JOB REGISTER</option>
+                                <option value="APPLICANT">APPLICANT</option>
+                            </select>
+                        </div>
+
+                        <div class="form-check d-flex justify-content-center mb-5">
+                            <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" required/>
+                            <label class="form-check-label" for="form2Example3">
+                            I agree all statements in <a href="#!">Terms of service</a>
+                            </label>
+                        </div>
+
+                        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                            <input type="submit" class="btn btn-primary btn-lg" name="r_btn">
+                        </div>
+
+                        </form>
+
                     </div>
+                    <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
 
-                    <div class="form-check d-flex justify-content-center mb-5">
-                        <select class="form-select"  name="role" aria-label="Default select example" required>
-                            <option selected>Create account as</option>
-                            <option value="1">ADMIN-JOB REGISTER</option>
-                            <option value="2">APPLICANT</option>
-                        </select>
                     </div>
-
-                    <div class="form-check d-flex justify-content-center mb-5">
-                        <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" required/>
-                        <label class="form-check-label" for="form2Example3">
-                        I agree all statements in <a href="#!">Terms of service</a>
-                        </label>
                     </div>
-
-                    <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <input type="submit" class="btn btn-primary btn-lg" name="r_btn">
-                    </div>
-
-                    </form>
-
-                </div>
-                <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
                 </div>
                 </div>
             </div>
             </div>
         </div>
-        </div>
-    </div>
-    </section>
-</body>
+        </section>
+    </body>
 </html>
 <?php
     require "conn_db.php";
