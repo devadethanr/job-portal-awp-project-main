54fc7b44647b0c5030134e4575a3f43140cb7203
devadethanr <125908755+devadethanr@users.noreply.github.com>
2023-08-20 19:35:14 +0530

    sign in and db page

--- signup.php
+++ signup.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>register-user</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </head>
 <body>
     <section class="vh-100" style="background-color: #eee;">
@@ -22,7 +24,7 @@
                     <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
-                        <input type="text" id="form3Example1c" class="form-control" />
+                        <input type="text" id="form3Example1c" name="name" class="form-control" required/>
                         <label class="form-label" for="form3Example1c">Your Name</label>
                         </div>
                     </div>
@@ -30,7 +32,7 @@
                     <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
-                        <input type="email" id="form3Example3c" class="form-control" />
+                        <input type="email" id="form3Example3c" name="mail" class="form-control" required/>
                         <label class="form-label" for="form3Example3c">Your Email</label>
                         </div>
                     </div>
@@ -38,7 +40,7 @@
                     <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
-                        <input type="password" id="form3Example4c" class="form-control" />
+                        <input type="password" id="form3Example4c" name="pass" class="form-control" required />
                         <label class="form-label" for="form3Example4c">Password</label>
                         </div>
                     </div>
@@ -46,29 +48,34 @@
                     <div class="d-flex flex-row align-items-center mb-4">
                         <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                         <div class="form-outline flex-fill mb-0">
-                        <input type="password" id="form3Example4cd" class="form-control" />
+                        <input type="password" id="form3Example4cd" name="rpass" class="form-control" required/>
                         <label class="form-label" for="form3Example4cd">Repeat your password</label>
                         </div>
                     </div>
 
                     <div class="form-check d-flex justify-content-center mb-5">
-                        <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+                        <select class="form-select"  name="role" aria-label="Default select example" required>
+                            <option selected>Create account as</option>
+                            <option value="1">ADMIN-JOB REGISTER</option>
+                            <option value="2">APPLICANT</option>
+                        </select>
+                    </div>
+
+                    <div class="form-check d-flex justify-content-center mb-5">
+                        <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" required/>
                         <label class="form-check-label" for="form2Example3">
                         I agree all statements in <a href="#!">Terms of service</a>
                         </label>
                     </div>
 
                     <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <button type="button" class="btn btn-primary btn-lg">Register</button>
+                        <input type="submit" class="btn btn-primary btn-lg" name="r_btn">
                     </div>
 
                     </form>
 
                 </div>
                 <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                    class="img-fluid" alt="Sample image">
 
                 </div>
                 </div>
@@ -80,3 +87,17 @@
     </section>
 </body>
 </html>
+<?php
+    require "conn_db.php";
+    if(isset($_POST['r_btn'])){
+        $name = $_POST['name'];
+        $email = $_POST['mail'];
+        $pass = $_POST['pass'];
+        $role = $_POST['role'];
+        $r_qry="INSERT INTO `register`(`name`, `mail`, `pass`, `role`) VALUES ('$name','$email','$pass','$role')";
+        $r_res=mysqli_query($conn,$r_qry);
+        if($r_res){
+            echo "<script> alert('registered successfully') </script>";
+        }
+    }
+?>
